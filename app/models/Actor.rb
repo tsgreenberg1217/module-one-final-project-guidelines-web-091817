9c05aaad4d50e07a2ebed15306a46ccf3aca1d8a
tsgreenberg1217 <tsgreenberg1217@gmail.com>
2017-10-02 16:03:56 -0400

    added table files

--- app/models/Actor.rb
+++ app/models/Actor.rb
@@ -4,7 +4,23 @@ require 'pry'
 
 # all_characters = RestClient.get('http://www.swapi.co/api/people/')
 # character_hash = JSON.parse(all_characters)
+byebug
+#
+# initial_link = 'http://chroniclingamerica.loc.gov/search/titles/results/?terms=michigan&format=json&page=1'
+# def get_api_info(link)
+#   api_info = JSON.parse(RestClient.get(link))
+# end
+# byebug
 
-api_info = JSON.parse(RestClient.get('http://chroniclingamerica.loc.gov/search/titles/results/?terms=michigan&format=json&page=1'))
 
-binding.pry
+uri = URI("https://api.nytimes.com/svc/search/v2/articlesearch.json")
+http = Net::HTTP.new(uri.host, uri.port)
+http.use_ssl = true
+uri.query = URI.encode_www_form({
+  "api-key" => "563720b6f34247bfb3a8f36ae853177a",
+  "page" => 1
+})
+request = Net::HTTP::Get.new(uri.request_uri)
+@result = JSON.parse(http.request(request).body)
+puts @result.inspect
+byebug
